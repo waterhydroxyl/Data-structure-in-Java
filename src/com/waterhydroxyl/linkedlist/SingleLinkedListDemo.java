d6ea5fc875c16deb1fa03185c8c5198d9235ab30
waterhydroxyl <waterhydroxyl@gmail.com>
2021-01-31 16:21:19 +0800

    Initial commit

---
+++ src/com/waterhydroxyl/linkedlist/SingleLinkedListDemo.java
@@ -0,0 +1,72 @@
+package com.waterhydroxyl.linkedlist;
+
+public class SingleLinkedListDemo {
+    public static void main(String[] args) {
+        HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
+        HeroNode hero2 = new HeroNode(2, "李白", "诗仙");
+        HeroNode hero3 = new HeroNode(3, "杜甫", "诗圣");
+        HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
+
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+
+        singleLinkedList.add(hero1);
+        singleLinkedList.add(hero2);
+        singleLinkedList.add(hero3);
+        singleLinkedList.add(hero4);
+
+        singleLinkedList.list();
+    }
+}
+
+class SingleLinkedList {
+    private HeroNode head = new HeroNode(0, "", "");
+
+    public void add(HeroNode heroNode) {
+        HeroNode temp = head;
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            temp = temp.next;
+        }
+        temp.next = heroNode;
+    }
+
+    public void list() {
+        if (head.next == null) {
+            System.out.println("列表空");
+            return;
+        }
+        HeroNode temp = head.next;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+}
+
+
+class HeroNode {
+    public int no;
+    public String name;
+    public String nickName;
+    public HeroNode next;
+
+    public HeroNode(int no, String name, String nickName) {
+        this.no = no;
+        this.name = name;
+        this.nickName = nickName;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                ", nickName='" + nickName + '\'' +
+                '}';
+    }
+}
