0de1eae713f01da6419b675e9b56fcf43a6d159a
waterhydroxyl <waterhydroxyl@gmail.com>
2021-03-14 20:32:26 +0800

    选择排序

--- src/com/waterhydroxyl/paixu/maopao.java
+++ src/com/waterhydroxyl/paixu/maopao.java
@@ -3,8 +3,24 @@ package com.waterhydroxyl.paixu;
 public class maopao {
     public static void main(String[] args) {
         int[] array = {2, -2, 5, 3, 7};
-        paixu(array);
+        insert(array);
         print(array);
+    }
+    public  static void insert(int[] array) {
+        int minIndex = 0;
+        int minValue = array[minIndex];
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i + 1; j <array.length ; j++) {
+                if (array[j] < minValue) {
+                    minIndex = j;
+                    minValue = array[j];
+                }
+            }
+            if (minValue != array[i]) {
+                array[minIndex] = array[i];
+                array[i] = minValue;
+            }
+        }
     }
 
     public static void paixu(int[] array) {
